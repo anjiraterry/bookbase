bb3c631749b1bb76ebe4d91fad22a2bbc1e02e25
Terry Agbo <anjiraterry@gmail.com>
2026-01-03 18:51:16 +0100

    Project reinitializatin

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'cxlgofyegcpxmlknvxqw.supabase.co',
+        hostname: 'zskuuerktoumtnccvizu.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
